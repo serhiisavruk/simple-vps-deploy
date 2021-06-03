701d8c0dd5dff19629d5de412b1d398ad636daf3
Vlad Zinko <vlad.zinko@contractors.medallia.com>
2021-06-03 22:15:41 +0300

    test

--- k8s/bin/helm-deploy.sh
+++ k8s/bin/helm-deploy.sh
@@ -6,4 +6,6 @@ mv ./k8s/chart/Chart.yaml ./k8s/chart/Chart.old.yaml &&
   echo -e "\r\nappVersion: \"v2\"\r\n" >> ./k8s/chart/Chart.yaml &&
   cat ./k8s/chart/Chart.yaml
 
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
 cd ./k8s/chart && helm dependency build
