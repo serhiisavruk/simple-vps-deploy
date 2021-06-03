de0cf29b8d472b76b957875185a110383cba8c3d
Vlad Zinko <vlad.zinko@contractors.medallia.com>
2021-06-03 22:08:26 +0300

    test

---
+++ k8s/bin/helm-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv ./k8s/chart/Chart.yaml ./k8s/chart/Chart.old.yaml &&
+  cat ./k8s/chart/Chart.old.yaml | grep -v appVersion > ./k8s/chart/Chart.yaml &&
+#  echo -e "\r\nappVersion: v${GITHUB_REF##*/}\r\n" >> ./.helm/app/Chart.yaml &&
+  echo -e "\r\nappVersion: \"v2\"\r\n" >> ./k8s/chart/Chart.yaml &&
+  cat ./k8s/chart/Chart.yaml
+
+cd ./k8s/chart && helm dependency build
